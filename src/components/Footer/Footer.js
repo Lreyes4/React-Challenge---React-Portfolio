58841fc4d43a775c4c945019b22c5cddf6b540ad
Lreyes4 <liliana_reyes0814@yahoo.com>
2022-09-28 02:43:27 +0300

    Footer.js added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/Morganbb104/HW_20_Portfolio_withReact'
+      href='https://github.com/Lreyes4/React-Challenge---React-Portfolio'
       className='link footer__link'
     >
-      Created By Cheng Chien Huang
+      A Project By Liliana Reyes
     </a>
   </footer>
 )
